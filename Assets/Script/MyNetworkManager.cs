c4a71e066eeb135d847e80a83474ea7023e60307
Lauriane <lauriane.loas@gmail.com>
2018-11-30 16:10:09 +0100

    test avec script

---
+++ Assets/Script/MyNetworkManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+        //server
+		if(Input.GetKeyDown(KeyCode.S))
+        {
+            StartServer();
+        }
+
+        //client
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartClient();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            StartHost();
+        }
+
+        
+
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("start server");
+    }
+
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        Debug.Log("start host");
+
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        base.OnStartClient(client);
+        Debug.Log("start client");
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnServerDisconnect(conn);
+        Debug.Log("quelqu'un s'est déconnecté");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        Debug.Log("client disconnect");
+
+    }
+
+
+}
